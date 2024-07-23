6d11e22a41bee63b565cb384a45701b7ed86026b
Carlos Andres Alzate <unarteman@gmail.com>
2024-07-23 13:37:16 -0300

    Cambio de rutas en los archivos tsconfig.json dentro de los diretorio start y end de cada modulo; Cambio de estructura en el arbol de archivos para que sea similar a los ejercicios de mslearn;

--- Kaz_type/module-01/m01-end/build/lab.js
+++ Kaz_type/module-01/m01-end/build/lab.js
@@ -1,20 +1,19 @@
-"use strict";
 // Variables
 // cCmo se declara una Variable
 // Hay 4 formas de declarar variables.
 // 1. tanto con el tipo, como con el valor inicial
-let empName_1 = 'John';
+var empName_1 = 'John';
 console.log(empName_1);
 // 2. solo el tipo
-let empName_2;
+var empName_2;
 // 3. solo el valor inicial
-let empName_3;
+var empName_3;
 // 4. sin el tipo y sin el valor inicial
-let name_1;
+var name_1;
 name_1 = 'Robert';
 // Como asignar un valor a una variable con el operador de asignación '='
-let empName_4 = 'John'; // Declaración
-let message = 'Hello World';
+var empName_4 = 'John'; // Declaración
+var message = 'Hello World';
 // diferencias entre var vs let vs const
 // var
 // diferencia #1 var se puede declarar sin inicializar
@@ -37,7 +36,7 @@ function example1() {
 // diferencia #1 let se puede declarar sin inicializar
 var bb;
 // diferencia #2
-let b = 'Hello World'; //inicializacion
+var b = 'Hello World'; //inicializacion
 console.log('valor de b:', b);
 b = 'Hello World'; // error: no puede ser actualizado
 // let b = 'Welcome to typescript'; // error: no se puede re-declarar
@@ -46,8 +45,8 @@ console.log('Actualizar el valor de b:', b);
 // diferencia #3
 function example2() {
     if (true) {
-        let x = 100;
-        console.log(x); // only block scoped
+        var x_1 = 100;
+        console.log(x_1); // only block scoped
     }
     // quitar el comentario en la linea siguiente
     // console.log(x); // error: la función no tiene un scoped
@@ -58,60 +57,60 @@ function example2() {
 // cons cc: number = 2.742;
 // console.log('constant value: ', c);
 // c = 1000; // error: no se puede actualizar el valor de una constante
-const c = 200; // error: no se puede volver a declarar una constante.
+var c = 200; // error: no se puede volver a declarar una constante.
 // diferencia #3
 function example3() {
     if (true) {
-        const x = 100;
-        console.log(x); // scope solo para este bloke
+        var x_2 = 100;
+        console.log(x_2); // scope solo para este bloke
     }
     // console.log(x); // error: no hay un scope en la función.
 }
 // Tipos de datos
 console.log('Tipos de Datos');
 // 'number'
-let firstNumber = 12.0;
-let secondNumber = 0x37cf;
-let thirdNumber = 0o377;
-let fourthNumber = 0b111000;
+var firstNumber = 12.0;
+var secondNumber = 0x37cf;
+var thirdNumber = 255;
+var fourthNumber = 56;
 console.log(firstNumber);
 console.log(secondNumber);
 console.log(thirdNumber);
 console.log(fourthNumber);
 // 'string'
-let empName11 = 'John';
-let empName12 = 'XXXX';
+var empName11 = 'John';
+var empName12 = 'XXXX';
 console.log(empName11);
 console.log(empName12);
 // 'boolean'
-let isPresent = true;
+var isPresent = true;
 console.log(isPresent);
 // 'void'
 /* void implica que no hay datos. Esto se usa típicamente para definir funciones */
 console.log("Type 'void'");
 // 'undefined'
-let x = undefined; // valor es 'undefined'
+var x = undefined; // valor es 'undefined'
 console.log(x);
 console.log(typeof x);
 x = 'hello';
 console.log(x);
 console.log(x);
-let x1; // el tipo de dato es 'undefined'
+var x1; // el tipo de dato es 'undefined'
 // x1 = "hello"; // Error:
 // 'null'
-let x2 = null; // el valor es 'null'
+var x2 = null; // el valor es 'null'
 console.log(x2);
 console.log(typeof x2);
 x2 = 'hello';
 console.log(x2);
 console.log(x2);
-let x22; // 'null' como tipo de dato
+var x22; // 'null' como tipo de dato
 // x22 = "hello"; // Error:
 // never
 // let str1: never = 'null'; // Error
 // 'any'
 console.log("Type 'any'");
-let value;
+var value;
 console.log(typeof value);
 value = 'Hello';
 console.log(value);
